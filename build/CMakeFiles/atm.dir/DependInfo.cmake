c50bfa94a0ced0a315d626abaa4df78c6392cff6
sgrabinski <sgrabinski@parasoft.com>
2021-07-29 08:23:51 +0200

    s

---
+++ build/CMakeFiles/atm.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/d/myTests/JenkinsPipeline_c++test_cmake/atm/ATM.cxx" "/cygdrive/d/myTests/JenkinsPipeline_c++test_cmake/atm/build/CMakeFiles/atm.dir/ATM.cxx.o"
+  "/cygdrive/d/myTests/JenkinsPipeline_c++test_cmake/atm/Account.cxx" "/cygdrive/d/myTests/JenkinsPipeline_c++test_cmake/atm/build/CMakeFiles/atm.dir/Account.cxx.o"
+  "/cygdrive/d/myTests/JenkinsPipeline_c++test_cmake/atm/Bank.cxx" "/cygdrive/d/myTests/JenkinsPipeline_c++test_cmake/atm/build/CMakeFiles/atm.dir/Bank.cxx.o"
+  "/cygdrive/d/myTests/JenkinsPipeline_c++test_cmake/atm/BaseDisplay.cxx" "/cygdrive/d/myTests/JenkinsPipeline_c++test_cmake/atm/build/CMakeFiles/atm.dir/BaseDisplay.cxx.o"
+  "/cygdrive/d/myTests/JenkinsPipeline_c++test_cmake/atm/Main.cxx" "/cygdrive/d/myTests/JenkinsPipeline_c++test_cmake/atm/build/CMakeFiles/atm.dir/Main.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/atm.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/atm.dir/ATM.cxx.o"
+  "CMakeFiles/atm.dir/Account.cxx.o"
+  "CMakeFiles/atm.dir/Bank.cxx.o"
+  "CMakeFiles/atm.dir/BaseDisplay.cxx.o"
+  "CMakeFiles/atm.dir/Main.cxx.o"
+  "atm.exe"
+  "atm.pdb"
+  "libatm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/atm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
